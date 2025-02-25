083c504dbb76cd2ea200fd64c9d4002e771ea0db
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2025-02-26 01:32:48 +0700

    remove view() and add deleteAny() in MediaPolicy

--- app/Policies/MediaPolicy.php
+++ app/Policies/MediaPolicy.php
@@ -13,33 +13,23 @@ class MediaPolicy
         return true;
     }
 
-    public function view(User $user, Media $media): bool
-    {
-        return false;
-    }
-
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     public function update(User $user, Media $media): bool
     {
-        return false;
+        return true;
     }
 
     public function delete(User $user, Media $media): bool
     {
-        return false;
+        return true;
     }
 
-    public function restore(User $user, Media $media): bool
+    public function deleteAny(User $user): bool
     {
-        return false;
-    }
-
-    public function forceDelete(User $user, Media $media): bool
-    {
-        return false;
+        return true;
     }
 }
